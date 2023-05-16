a5e9065d197fe01f998c4a45408f35f16c757c9f
Pawel1894 <pawepohl@gmail.com>
2023-05-16 20:20:43 +0200

    Display info if no activities are coming

--- src/components/IncomingActivities.tsx
+++ src/components/IncomingActivities.tsx
@@ -13,81 +13,83 @@ export default function IncomingActivities() {
     <Box p={2} height={264}>
       <Typography>Incoming activities</Typography>
       <List style={{ height: "93%", overflow: "auto" }}>
-        {data && data.length > 0
-          ? data.map((item) => {
-              return (
-                <ListItemText
-                  sx={{
-                    marginTop: "1rem",
-                  }}
-                  key={item.id}
-                  primary={
-                    <>
-                      <Grid container columns={13}>
-                        <Grid xs={3}>
-                          <Typography color={"primary.dark"} component="span" variant="caption">
-                            Title
-                          </Typography>
-                          <br />
-                          <Typography component="span" variant="body2">
-                            {`${item.title ?? ""}`}
-                          </Typography>
-                        </Grid>
-                        <Grid xs={3}>
-                          <Typography color={"primary.dark"} component="span" variant="caption">
-                            Status
-                          </Typography>
-                          <br />
-                          <Typography component="span" variant="body2">
-                            {`${item.status ?? ""}`}
-                          </Typography>
-                        </Grid>
-                        <Grid xs={3}>
-                          <Typography color={"primary.dark"} component="span" variant="caption">
-                            Due to
-                          </Typography>
-                          <br />
-                          <Typography component="span" variant="body2">
-                            {`${dayjs(item.date?.toString()).format("DD/MM/YYYY")} ${dayjs(
-                              item.time?.toString()
-                            ).format("HH:mm:ss")}`}
-                          </Typography>
-                        </Grid>
-                        <Grid xs={3}>
-                          <Typography color={"primary.dark"} component="span" variant="caption">
-                            {item.contact ? "Contact" : "Lead"}
-                          </Typography>
-                          <br />
-                          <Typography component="span" variant="body2">
-                            {item.contact ? (
-                              <Link
-                                color={"text.primary"}
-                                href={`/contact/${item.contact.id}`}
-                              >{`${item.contact.firstName} ${item.contact.lastName}`}</Link>
-                            ) : null}
-                            {item.lead ? (
-                              <Link
-                                color={"text.primary"}
-                                href={`/lead/${item.lead.id}`}
-                              >{`${item.lead.firstName} ${item.lead.lastName}`}</Link>
-                            ) : null}
-                          </Typography>
-                        </Grid>
-                        <Grid xs={1}>
-                          {item.id ? (
-                            <IconButton href={`activity/${item.id}`} title="View">
-                              <Visibility />
-                            </IconButton>
-                          ) : null}
-                        </Grid>
+        {data && data.length > 0 ? (
+          data.map((item) => {
+            return (
+              <ListItemText
+                sx={{
+                  marginTop: "1rem",
+                }}
+                key={item.id}
+                primary={
+                  <>
+                    <Grid container columns={13}>
+                      <Grid xs={3}>
+                        <Typography color={"primary.dark"} component="span" variant="caption">
+                          Title
+                        </Typography>
+                        <br />
+                        <Typography component="span" variant="body2">
+                          {`${item.title ?? ""}`}
+                        </Typography>
                       </Grid>
-                      <Divider sx={{ marginTop: "1rem" }} />
-                    </>
-                  }
-                />
-              );
-            })
-          : null}
+                      <Grid xs={3}>
+                        <Typography color={"primary.dark"} component="span" variant="caption">
+                          Status
+                        </Typography>
+                        <br />
+                        <Typography component="span" variant="body2">
+                          {`${item.status ?? ""}`}
+                        </Typography>
+                      </Grid>
+                      <Grid xs={3}>
+                        <Typography color={"primary.dark"} component="span" variant="caption">
+                          Due to
+                        </Typography>
+                        <br />
+                        <Typography component="span" variant="body2">
+                          {`${dayjs(item.date?.toString()).format("DD/MM/YYYY")} ${dayjs(
+                            item.time?.toString()
+                          ).format("HH:mm:ss")}`}
+                        </Typography>
+                      </Grid>
+                      <Grid xs={3}>
+                        <Typography color={"primary.dark"} component="span" variant="caption">
+                          {item.contact ? "Contact" : "Lead"}
+                        </Typography>
+                        <br />
+                        <Typography component="span" variant="body2">
+                          {item.contact ? (
+                            <Link
+                              color={"text.primary"}
+                              href={`/contact/${item.contact.id}`}
+                            >{`${item.contact.firstName} ${item.contact.lastName}`}</Link>
+                          ) : null}
+                          {item.lead ? (
+                            <Link
+                              color={"text.primary"}
+                              href={`/lead/${item.lead.id}`}
+                            >{`${item.lead.firstName} ${item.lead.lastName}`}</Link>
+                          ) : null}
+                        </Typography>
+                      </Grid>
+                      <Grid xs={1}>
+                        {item.id ? (
+                          <IconButton href={`activity/${item.id}`} title="View">
+                            <Visibility />
+                          </IconButton>
+                        ) : null}
+                      </Grid>
+                    </Grid>
+                    <Divider sx={{ marginTop: "1rem" }} />
+                  </>
+                }
+              />
+            );
+          })
+        ) : (
+          <span>No activities coming soon</span>
+        )}
       </List>
     </Box>
   );
